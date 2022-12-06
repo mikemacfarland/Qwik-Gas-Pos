e304ac5b19fcdd4fbad24c2ecf4ec444949e23eb
mike macfarland <m_macfarland@live.com>
2022-12-05 19:23:19 -0500

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      Gas / home screen
+      <Link class="mindblow" href="/flower/">
+        Blow my mind 🤯
+      </Link>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};
