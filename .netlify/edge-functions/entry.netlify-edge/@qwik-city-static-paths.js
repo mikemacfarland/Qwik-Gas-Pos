9be4e13e57e2782d452b1977d6ceb6634902a01e
mike macfarland <m_macfarland@live.com>
2022-12-14 12:44:00 -0500

    merged build into development

---
+++ .netlify/edge-functions/entry.netlify-edge/@qwik-city-static-paths.js
@@ -0,0 +1,14 @@
+const staticPaths = new Set(["/","/_headers","/favicon.svg","/food-drinks/","/manifest.json","/q-manifest.json","/robots.txt","/service-worker.js","/settings/","/sitemap.xml","/tobacco/","/user/"]);
+function isStaticPath(p) {
+  if (p.startsWith("/build/")) {
+    return true;
+  }
+  if (p.startsWith("/assets/")) {
+    return true;
+  }
+  if (staticPaths.has(p)) {
+    return true;
+  }
+  return false;
+}
+export { isStaticPath };
